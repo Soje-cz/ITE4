1eb40d713b9de373442ab786606e74fa0be7b7a7
Daniel Miťko <129744420+DanielMitko@users.noreply.github.com>
2024-03-20 10:23:16 +0100

    Změna dvě
    s

--- Snake/Snake.py
+++ Snake/Snake.py
@@ -1,1 +1,20 @@
-print("hello")
+import pyglet
+from pyglet.window import key
+import random
+
+# Velikost okna
+WIDTH = 1920
+HEIGHT = 1080
+
+# Velikost hada
+BLOCK_SIZE = 20
+
+# Rychlost hada
+SNAKE_SPEED = 0.5
+
+# Inicializace okna
+window = pyglet.window.Window(WIDTH, HEIGHT)
+
+# Inicializace hada
+snake = [(WIDTH // 2, HEIGHT // 2)]
+snake_direction = key.RIGHT
