4ec13063ff0c4d47f80c293923dc6d3fc2c102da
Král bez hradu <129742077+MichalMetzner@users.noreply.github.com>
2024-03-20 11:04:19 +0100

    Update piskvorky.py

--- piskvorky/Hra/piskvorky.py
+++ piskvorky/Hra/piskvorky.py
@@ -1,1 +1,25 @@
-.
+from tkinter import *
+import random
+
+# Globální proměnné
+hráči = ["X", "O"]
+hráč = random.choice(hráči)
+tlačítka = [[0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0]]
+kola = {hráči[0]: 0, hráči[1]: 0}
+celková_kola = 3
+odehraná_kola = 0
+pauza_kola = False
+
+# Funkce pro kontrolu prázdných polí na hrací desce
+def prázdná_pole():
+    počet_prázdných = 25
+    for řádek in range(5):
+        for sloupec in range(5):
+            if tlačítka[řádek][sloupec]['text'] != "":
+                počet_prázdných -= 1
+    return počet_prázdných > 0
+
