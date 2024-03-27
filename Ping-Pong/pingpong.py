5db1f10c80ace6eeb250c5457b52eb5d4a62b508
Honziiik                                                      (beum caujte lidi tady gogo) <navratilj@soje.cz>
2024-03-27 10:45:02 +0100

    Update pingpong.py

--- Ping-Pong/pingpong.py
+++ Ping-Pong/pingpong.py
@@ -49,10 +49,52 @@ def reset():
     rychlost_mice[1] = random.uniform(-1, 1) * RYCHLOST
 
 
+#funkce pro pohybu micku a pricitani skore
+def obnov_stav(dt):
+
+    pozice_mice[0] += rychlost_mice[0] * dt
+    pozice_mice[1] += rychlost_mice[1] * dt
+
+    if pozice_mice[1] < VELIKOST_MICE // 2:
+        rychlost_mice[1] = abs(rychlost_mice[1])
+
+    if pozice_mice[1] > SIRKA - VELIKOST_MICE // 2:
+        rychlost_mice[1] = -abs(rychlost_mice[1])
+
+    for cislo_palky in (0, 1):
+        if ('doprava', cislo_palky) in stisknute_klavesy:
+            pozice_palek[cislo_palky] += RYCHLOST_PALKY * dt
+        if ('doleva', cislo_palky) in stisknute_klavesy:
+            pozice_palek[cislo_palky] -= RYCHLOST_PALKY * dt
+
+        if pozice_palek[cislo_palky] < DELKA_PALKY / 2:
+            pozice_palek[cislo_palky] = DELKA_PALKY / 2
+        if pozice_palek[cislo_palky] > SIRKA - DELKA_PALKY / 2:
+            pozice_palek[cislo_palky] = SIRKA - DELKA_PALKY / 2
+
+    palka_min = pozice_mice[1] - VELIKOST_MICE/2 - DELKA_PALKY/2
+    palka_max = pozice_mice[1] + VELIKOST_MICE/2 + DELKA_PALKY/2
+
+    if pozice_mice[0] < TLOUSTKA_PALKY + VELIKOST_MICE / 2:
+        if palka_min < pozice_palek[0] < palka_max:
+            rychlost_mice[0] = abs(rychlost_mice[0])
+        else:
+            skore[1] += 1
+            reset()
+
+    if pozice_mice[0] > SIRKA - (TLOUSTKA_PALKY + VELIKOST_MICE / 2):
+        if palka_min < pozice_palek[1] < palka_max:
+            rychlost_mice[0] = -abs(rychlost_mice[0])
+        else:
+            skore[0] += 1
+            reset()
+
+
 #funkce pro kresleni palek, micku
 def nakresli_obdelnik(x, y, sirka, vyska):
     obdelnik = pyglet.shapes.Rectangle(x=y, y=x, width=vyska, height=sirka)
     obdelnik.draw()
+
 
 #funkce pro kreselni skore
 def nakresli_text(text, x, y, pozice_y):
@@ -63,7 +105,76 @@ def nakresli_text(text, x, y, pozice_y):
     napis.draw()
 
 
+#vykresleni vsech funkci
+def vykresli():
+    window.clear() 
+
+    #mic
+    nakresli_obdelnik(
+        pozice_mice[0] - VELIKOST_MICE // 2,
+        pozice_mice[1] - VELIKOST_MICE // 2,
+        VELIKOST_MICE,
+        VELIKOST_MICE)
+
+    #palky
+    for x, y in [(0, pozice_palek[0]), (SIRKA, pozice_palek[1])] :
+        nakresli_obdelnik(
+            x - TLOUSTKA_PALKY,
+            y - DELKA_PALKY // 2,
+            TLOUSTKA_PALKY * 2,
+            DELKA_PALKY)
+
+    #pulici cara
+    for x in range(DELKA_PULICI_CARKY // 2, SIRKA, DELKA_PULICI_CARKY * 2):
+        nakresli_obdelnik(
+            SIRKA // 2 - 1,
+            x,
+            2,
+            DELKA_PULICI_CARKY)
+
+    #skore
+    nakresli_text(str(skore[0]),
+                  x=ODSAZENI_TEXTU,
+                  y=SIRKA - ODSAZENI_TEXTU - VELIKOST_FONTU,
+                  pozice_y='top')
+
+    nakresli_text(str(skore[1]),
+                  x=SIRKA - ODSAZENI_TEXTU,
+                  y=SIRKA - ODSAZENI_TEXTU - VELIKOST_FONTU,
+                  pozice_y='bottom')
 
 
+#funkce pro fungovani tracitek - stisk
+def stisk_klavesy(symbol, modifikatory):
+    if symbol == key.D:
+        stisknute_klavesy.add(('doprava', 0))
+    if symbol == key.A:
+        stisknute_klavesy.add(('doleva', 0))
+    if symbol == key.RIGHT:
+        stisknute_klavesy.add(('doprava', 1))
+    if symbol == key.LEFT:
+        stisknute_klavesy.add(('doleva', 1))
 
+#funkce pro fungovani tracitek - pust
+def pusteni_klavesy(symbol, modifikatory):
+    if symbol == key.D:
+        stisknute_klavesy.discard(('doprava', 0))
+    if symbol == key.A:
+        stisknute_klavesy.discard(('doleva', 0))
+    if symbol == key.RIGHT:
+        stisknute_klavesy.discard(('doprava', 1))
+    if symbol == key.LEFT:
+        stisknute_klavesy.discard(('doleva', 1))
+
+
+#volani vsech funkci a spusteni programu
+reset()
+window = pyglet.window.Window(width=SIRKA, height=SIRKA)
+window.push_handlers(
+    on_draw=vykresli,  
+    on_key_press=stisk_klavesy,  
+    on_key_release=pusteni_klavesy, 
+    )
+
+pyglet.clock.schedule(obnov_stav)
 pyglet.app.run()
